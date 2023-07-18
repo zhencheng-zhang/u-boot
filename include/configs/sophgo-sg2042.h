def5c5ed23da2f799f4d2480483cc9f75303dcb6
zhencheng zhang <zhencheng.zhang@sophgo.com>
2023-07-18 09:57:25 +0800

    update sophgo_sg2042_defconfig and sophgo-sg2042.h to support loading grub2
    Signed-off-by: zhencheng zhang <zhencheng.zhang@sophgo.com>

--- include/configs/sophgo-sg2042.h
+++ include/configs/sophgo-sg2042.h
@@ -33,12 +33,6 @@
 	"ramdisk_addr_r=0x8d300000\0" \
 	"fdt_addr_r=0x8e000000\0" \
 	"fdt_addr_fixed=0x20000000\0" \
-	"scriptaddr=0x8e400000\0" \
-	"scriptfile=uEnv_sophgo_sg2042.txt\0" \
-	"devtype=mmc\0" \
-	"devnum=0\0" \
-	"bootpart=2\0" \
-	"bootcmd=ext4load mmc ${devnum}:${bootpart} ${scriptaddr} ${scriptfile}; env import -t ${scriptaddr} ${filesize};boot\0" \
 	"PS1=[Sophgo]# \0" \
 	BOOTENV
 
